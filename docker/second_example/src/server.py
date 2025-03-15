ca3ca99a51d50b0d9db658f52a392a02067809ad
sala104 <sala104@poliacademico.edu.loc>
2025-03-15 10:59:10 -0500

    Third example

--- docker/second_example/src/server.py
+++ docker/second_example/src/server.py
@@ -1,4 +1,4 @@
-from flask import Flask,request
+from flask import Flask, request, jsonify
 import boto3
 
 server = Flask(__name__)
@@ -7,8 +7,18 @@ s3 = boto3.client('s3')
 
 @server.route("/")
 def list_buckets():
+    query = request.args.get('name')  
+    
+    if query:
+        try:
+            s3.head_bucket(Bucket=query) 
+            return jsonify({"Bucket": query})
+        except s3.exceptions.ClientError:
+            return jsonify({"Error": "Bucket not found"}), 404
+    
     response = s3.list_buckets()
-    return response
+    bucket_names = [bucket['Name'] for bucket in response.get('Buckets', [])]
+    return jsonify({"Buckets": bucket_names})
 
 if __name__ == "__main__":
-    server.run(host='0.0.0.0')
+    server.run(host='0.0.0.0', port=5000, debug=True)
---
+++ docker/third_example/src/server.py
@@ -0,0 +1,14 @@
+from flask import Flask,request
+import boto3
+
+server = Flask(__name__)
+
+s3 = boto3.client('s3')
+
+@server.route("/")
+def list_buckets():
+    response = s3.list_buckets()
+    return response
+
+if __name__ == "__main__":
+    server.run(host='0.0.0.0')
